c8318c253204627e97e3af44e5299e92af7c92b7
Maxim Fainberg <maksym.fainberg@mobenga.com>
2019-04-30 15:54:46 +0200

    Day 2

--- src/adv/a.clj
+++ src/adv/a.clj
@@ -10,10 +10,11 @@
 (defn split-lines [text]
   (split text #"\n"))
 
-; DAY 1 - part 1
+; DAY 1
 (def input1 (slurp "data/input1.txt"))
 
-(defn day1 
+; DAY 1 - part 1
+(defn day1
   ([s] (let [freq (frequencies s)]
          (- (get freq \( 0)
             (get freq \) 0)))) 
@@ -21,7 +22,7 @@
   ([] (day1 input1)))
 
 ; DAY 1 - part 2
-(defn day2
+(defn day1-2
   ([s] (->> s
             (map #(if (= % \() 1 -1))
             (reductions +)
@@ -29,7 +30,7 @@
             (count)
             (inc)))
            
-  ([] (day2 input1)))
+  ([] (day1-2 input1)))
 
 ; DAY 2
 
@@ -44,5 +45,31 @@
        (split-lines) 
        (map parse-dim-line)))
 
+; DAY 2 - part 1
 
+(defn get-total-wrapping [dimensions] 
+  (let [[a b c] (sort dimensions)]
+    (+ (* 2 (+ (* a b) (* a c) (* b c))) 
+       (* a b))))
+
+(defn day2 
+  ([presents] (->> presents
+                   (map get-total-wrapping)
+                   (reduce +)))
+  ([] (day2 input2))) 
+
+; DAY2 - part 2
+
+(defn get-total-ribbon [dimensions] 
+  (let [[a b c] (sort dimensions)]
+    (+ (* a b c)
+       (* 2 (+ a b)))))
+
+
+(defn day2-2
+  ([presents]  (->> presents
+                    (map get-total-ribbon)
+                    (reduce +)))
+
+  ([] (day2-2 input2)))
 
