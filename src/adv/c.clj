9e78eae3285c41bef264dad1c52d5f4d2b4662ed
Maxim Fainberg <maksym.fainberg@mobenga.com>
2019-05-07 11:21:09 +0200

    Day 8 - part 1

--- src/adv/c.clj
+++ src/adv/c.clj
@@ -6,5 +6,24 @@
   (->> "data/input8.txt"
        (slurp)
        (split-lines)))
- 
 
+(defn get-in-memory-size [s]
+  (let [patterns (re-seq #"\\." s)]
+    (- (count s)
+       (reduce + 
+               2
+               (map #(case (second %) 
+                      \\ 1
+                      \" 1
+                      3)
+                      patterns)))))
+
+(defn get-code-representation-size [s]
+  (count s))
+
+(defn day8
+  ([lines] (->> lines
+                (map #(- (get-code-representation-size %) (get-in-memory-size %)))
+                (reduce + 0)))
+  ([] (day8 input8)))
+ 
