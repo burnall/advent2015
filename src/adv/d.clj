7e0f6dfa308764bde6c61569eee600ce6de995ea
Maxim Fainberg <maksym.fainberg@mobenga.com>
2019-05-14 09:17:22 +0200

    Day 12 - part 2. JSON parser

--- src/adv/d.clj
+++ src/adv/d.clj
@@ -83,3 +83,86 @@
   ([json] (json-sum json))
   ([] (day12 input12)))
 
+; Day 12 - part 2
+(defn char-to-int [c] (- (int c) (int \0)))
+
+(defn digit? [c] (and (>= (int c) (int \0)) (<= (int c) (int \9)))) 
+
+(defn parse-integer-int [acc xs]
+  (if (digit? (first xs))
+    (recur (+ (* 10 acc) (char-to-int (first xs))) (rest xs))
+    {:rst xs, :value acc}))
+
+(defn parse-integer [negative? acc xs] 
+  (let [{xs :rst, value :value} (parse-integer-int acc xs)]
+    {:rst xs, :value ((if negative? - identity) value)})) 
+
+(defn parse-string-int [acc [x & xs]]
+  (if (= x \")
+    {:rst xs, :value acc}
+    (recur (conj acc x) xs)))
+
+(defn parse-string [xs]
+  (let [{rst :rst, value :value} (parse-string-int [] xs)]
+    {:rst rst, :value (apply str value)}))
+
+(declare parse-object-int)
+(declare parse-array-int)
+
+(defn parse-value [x xs]
+  (cond
+    (digit? x) (parse-integer false (char-to-int x) xs)
+    (= x \-) (parse-integer true 0 xs)
+    (= x \{) (parse-object-int {} xs)
+    (= x \[) (parse-array-int [] xs)
+    (= x \") (parse-string xs)))
+
+(defn parse-array-int [acc [x & xs]]
+  (if (= x \]) 
+    {:rst xs, :value acc}
+    (let [{[x & xs] :rst, value :value} (parse-value x xs)
+          acc (conj acc value)]
+      (if (= x \,)
+        (recur acc xs)
+        {:rst xs, :value acc})))) ; Skip implied ']'
+
+(defn parse-object-int [acc [x & xs]] 
+  (if (= x \})
+    {:rst xs, :value acc}
+    (let [{xs :rst, property-key :value} (parse-string xs) ; Ignore x, should be '"'
+          [x & xs] (rest xs) ; Skip implied ':' 
+          {[x & xs] :rst, property-value :value} (parse-value x xs)
+          acc (assoc acc property-key property-value)]
+      (if (= x \,)
+        (recur acc xs)
+        {:rst xs, :value acc})))) ; Skip implied '}'
+
+(defn parse-json [[x & xs]]
+  (condp = x 
+    \{ (parse-object-int {} xs)
+    \[ (parse-array-int [] xs)))
+
+(defn has-red? [obj]
+  (->> obj
+      (keys)
+      (filter (fn [a-key] (= (obj a-key) "red")))
+      (seq)))
+
+(defn traverse-json [json]
+  (cond 
+    (int? json) json
+    (string? json) 0
+    (vector? json) (reduce + (map traverse-json json))
+    (map? json) 
+      (if (has-red? json) 
+        0
+        (reduce + (map #(traverse-json (json %)) (keys json)))))) 
+
+(defn day12-2
+  ([json-string] 
+    (->> json-string
+         (parse-json)
+         (:value)
+         (traverse-json)))
+  ([] (day12-2 input12)))
+  
