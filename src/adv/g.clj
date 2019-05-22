78c7f3fb324d4fa6b5eb3f3bc6ccb6391c1e2b8d
Maxim Fainberg <maksym.fainberg@mobenga.com>
2019-05-22 09:10:48 +0200

    Day 17 - part 2

--- src/adv/g.clj
+++ src/adv/g.clj
@@ -19,4 +19,18 @@
   ([jars vol] (split-volume jars vol))
 
   ([] (day17 input17 150)))
-  
+
+; Part 2
+(defn split-volume-2 [[jar & more] vol used-jar]
+  (cond
+    (= vol 0) [used-jar]
+    (nil? jar) []
+    (< vol 0) []
+    :else (concat (split-volume-2 more vol used-jar)
+                  (split-volume-2 more (- vol jar) (inc used-jar)))))
+
+(defn day17-2
+  ([jars vol] (frequencies (split-volume-2 jars vol 0)))
+
+  ([] (day17-2 input17 150)))
+ 
