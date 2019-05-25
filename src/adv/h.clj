87a623e37f49fda83c9fb86c9e74c7ed301d78cc
Maxim Fainberg <maksym.fainberg@mobenga.com>
2019-05-25 21:53:41 +0200

    Day 19

--- src/adv/h.clj
+++ src/adv/h.clj
@@ -6,22 +6,27 @@
        (re-seq #"([A-Z][a-z]*)")
        (mapv first)))
 
-(defn get-ruleset [xs]
+(defn get-rules [xs]
   (->> xs 
        (map (partial re-seq #"(.+) => (.+)"))
        (map first)
-       (map (fn [[_ from to]] {:from from, :to (parse-molecule to)}))
+       (map (fn [[_ from to]] {:from from, :to (parse-molecule to)}))))
+ 
+(defn get-ruleset [rules]
+  (->> rules
        (group-by :from)
        (map (fn [[from objs]] [from (map :to objs)]))
        (into {})))
 
 (defn build-config [xs]
-  (let [last-index (dec (count xs))]
+  (let [last-index (dec (count xs))
+        rules (get-rules (subvec xs 0 (dec last-index)))]
     {:molecule (parse-molecule (xs last-index))
-     :ruleset (get-ruleset (subvec xs 0 (dec last-index)))})) 
+     :rules rules
+     :ruleset (get-ruleset rules)}))
 
 (def input19
-  (->> "data/input19-small.txt"
+  (->> "data/input19.txt"
        (slurp)
        (split-lines)
        (build-config)))
@@ -44,7 +49,6 @@
   ([] (day19 input19)))
 
 ; Part 2
-
 (defn mutate-many [ruleset molecules]
   (->> molecules
        (mapcat (partial mutate ruleset))
@@ -60,8 +64,48 @@
        (filter boolean)
        (first)))
 
+(defn match-rule? [molecule {to :to}] 
+  (and (>= (count molecule) (count to))
+       (= (subvec molecule 0 (count to))  
+          to)))
+
+(defn mutate-back [molecule i {from :from, to :to}]
+  (->> [(subvec molecule 0 i)
+        [from]
+        (subvec molecule (+ i (count to)))]
+        (apply concat)
+        (vec))) 
+
+(defn mutate-back-position [rules molecule i]
+  (let [sub-molecule (subvec molecule i)]
+    (->> rules
+         (filter (partial match-rule? sub-molecule))
+         (map (partial mutate-back molecule i)))))
+
+(defn back-track-molecule [rules molecule]
+  (->> molecule
+       (count)
+       (range)
+       (mapcat (partial mutate-back-position rules molecule))))
+
+(defn back-track-molecules [rules molecules]
+  (->> molecules
+       (mapcat (partial back-track-molecule rules))
+       (distinct)))
+
+; Also slow brute-force solution
+(defn back-track-molecule-to-e [rules molecule]
+  (->> [molecule]
+       (iterate (partial back-track-molecules rules))
+       (map (fn [i molecules] 
+              (and (seq (filter (partial = ["e"])  molecules)) i)) 
+            (range))
+       (filter boolean)
+       (first)))
+
 (defn day19-2 
+  ([{:keys [rules molecule]}] (back-track-molecule-to-e rules molecule))
   ([] (day19-2 input19)))
 
-
+; Can be solved with no code - considering very particular data sample
 
