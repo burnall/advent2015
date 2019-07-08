b662f5f4c39e1d2fefc9108d5607d6e4eadf5a64
Maxim Fainberg <maksym.fainberg@mobenga.com>
2019-07-08 14:16:44 +0200

    Day 22 - general structure

--- src/adv/k.clj
+++ src/adv/k.clj
@@ -1,45 +1,70 @@
 (ns adv.k)
 
 (def spells [
-  {:id 1, :name "magic missile", :mana 53,  :damage 4, :effect 0}
-  {:id 2, :name "drain", :mana 73, :damage 2, :effect 0, :heal 2}
-  {:id 3, :name "shield", :mana 113, :armor-inc 7, :effect 6}
-  {:id 4, :name "poison", :mana 173, :damage 3, :effect 6}
-  {:id 5, :name "recharge", :mana 229, :mana-inc 101, :effect 5}])
+  {:id 1, :name "magic missile", :mana 53,  :damage 4 :heal 0}
+  {:id 2, :name "drain", :mana 73, :damage 2, :heal 2}
+  ;{:id 3, :name "shield", :mana 113, :armor-inc 7, :effect 6}
+  ;{:id 4, :name "poison", :mana 173, :damage 3, :effect 6}
+  ;{:id 5, :name "recharge", :mana 229, :mana-inc 101, :effect 5}
+  ])
 
-(defn initial-state [boss player]
+(defn get-initial-state [boss player]
   {:turn :player
-   :mana-spent 0 
    :boss boss
-   :player player
+   :player (merge player {:mana-spent 0, :moves []})
    :effects #{}})
 
-(defn apply-armor-inc [spell [boss player]]
-  [boss player]
-)
+(defn do-player-spell [{player :player, boss :boss, moves :moves, :as state} 
+                   current-best 
+                   {:keys [heal damage mana effect] :as spell}]
+  (when (nil? effect)  
+    (let [player-changes {:hp (+ heal (:hp player))
+                          :mana-left (- (:mana-left player) mana)
+                          :mana-spent (+ (:mana-spent player) mana)
+                          :moves (conj moves spell)}
+          boss (update boss :hp (partial + (- damage)))
+          state (merge state {:boss boss, :player player, :turn :boss})]
+      (find-best-scenario current-best state))))   
 
-(defn apply-effect [[boss player] spell]
-  (prn boss player spell)
-  (->> [boss player]
-       (apply-armor-inc spell)))
+(defn get-damage [damage armor]
+  (if (>= armor damage)
+    1
+    (- damage armor))) 
 
-(defn apply-effects [{:keys [boss player effects]}]
-  (prn boss player effects)
-  (reduce apply-effect 
-      [boss player]
-       effects))
+(defn apply-effects [{:keys [player boss effects] :as state}]
+  state)
 
-(defn search-best-variation [{:keys [min-mana-spent cnt]} {:keys [mana-spent turn boss player effects] :as state}]
+(defn do-boss-turn [current-best state] 
+  (let [{:keys [player boss]} (apply-effects state)
+        damage (get-damage (:damage boss) (:armor player))
+        player (update player :hp (partial + (- damage)))
+        state (merge state {:player player, :boss boss, :turn :player})] 
+    (find-best-scenario current-best state)))      
+
+(defn do-player-turn [current-best state] 
+  (let [{:keys [player boss]} (apply-effects state)]
+    (->> spells 
+         (reduce (partial do-player-spell state) 
+                 current-best))))
+
+(defn branch [current-best state]
+  (if (= :boss (:turn state))
+    (do-boss-turn current-best state)
+    (do-player-turn current-best state)))
+
+(defn find-best-scenario [{:keys [min-mana-spent moves] :as current-best} 
+                          {:keys [turn boss player effects] :as state}]
+  (prn state current-best)
   (cond 
-    ;(>= mana-spent min-mana-spent) min-mana-spent
-    ;(<= (:hp player) 0) min-mana-spent
-    ;(<= (:hp boss) 0) mana-spent
-    :else (apply-effects (assoc state :effects #{3 4 5}))
-))
-
+    (>= (:mana-spent player) min-mana-spent) current-best
+    (<= (:hp player) 0) current-best
+    (<= (:hp boss) 0) {:min-mana-spent (:mana-spent player), :moves moves} 
+    :else (branch current-best state)))
+     
 (defn day22
   ([boss player] 
-    (search-best-variation {:cnt 0, :min-mana-spent Integer/MAX_VALUE}
-                           (initial-state boss player)))
+    (find-best-scenario {:moves [], :min-mana-spent Integer/MAX_VALUE}
+                         (get-initial-state boss player)))
 
-  ([] (day22 {:hp 58, :damage 9} {:hp 50, :mana 500, :armor 0})))
+  ([] (day22 {:hp 58, :damage 9} {:hp 50, :mana-left 500, :armor 0})))
+
