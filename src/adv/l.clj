598e3cb76995e160068ae5c99d72b898f3f704ef
Maxim Fainberg <maksym.fainberg@mobenga.com>
2019-07-13 00:47:48 +0200

    Day 23

--- src/adv/l.clj
+++ src/adv/l.clj
@@ -8,21 +8,45 @@
     (parse-int s)
     (keyword s)))
 
-(defn make-command 
-  ([cmd op1] {:cmd cmd, :op1 (get-value op1)})
-  ([cmd op1 op2] (assoc (make-command cmd op1) :op2 (get-value op2)))) 
+(defn make-instruction 
+  ([cmd op1] {:cmd (keyword cmd), :op1 (get-value op1)})
+  ([cmd op1 op2] (assoc (make-instruction cmd op1) :op2 (get-value op2)))) 
 
-(defn parse-command [line]
+(defn parse-instruction [line]
   (-> line 
       (split #"[ ,]+") 
-      (#(apply make-command %)))) 
+      (#(apply make-instruction %)))) 
 
 (def input23 
   (->> "data/input23.txt"
        (slurp)
        (split-lines)
-       (map parse-command)))
+       (mapv parse-instruction)))
 
+(defn exec-cmd [cmds {line :line, :as state}] 
+  (prn state)
+  (let [{:keys [cmd op1 op2]} (cmds line)]
+    (case cmd
+      :hlf (merge state {op1 (quot (op1 state) 2), :line (inc line)})
+      :tpl (merge state {op1 (* 3 (op1 state)), :line (inc line)})
+      :inc (merge state {op1 (inc (op1 state)), :line (inc line)})
+      :jmp (assoc state :line (+ line op1))
+      :jie (assoc state :line 
+                  (if (zero? (mod (op1 state) 2))
+                    (+ line op2)
+                    (inc line)))
+      :jio (assoc state :line 
+                  (if (= 1 (op1 state))
+                    (+ line op2)
+                    (inc line))))))
+
+(defn run-program
+  ([cmds] (->> {:a 1N, :b 0N, :line 0}
+               (iterate (partial exec-cmd cmds))
+               (drop-while #(and (>= (:line %) 0) (> (count cmds) (:line %))))
+               (first)))
+
+  ([] (run-program input23)))
 
 
 
