25ad86966ae995d617ed12e1cad8d34a5afb27af
Maxim Fainberg <maksym.fainberg@mobenga.com>
2019-07-16 23:57:23 +0200

    Day 24 - part 1

--- src/adv/m.clj
+++ src/adv/m.clj
@@ -7,30 +7,83 @@
   (->> "data/input24.txt"
        (slurp)
        (split-lines)
-       (mapv parse-int)
+       (map parse-int)
        (reverse)
+       (vec)
        ))
 
-(defn better-solution? [{:keys [cnt-a qe-a]} 
-                       {:keys [cnt-b qe-b]}]
+(defn better-solution? [{cnt-a :cnt, qe-a :qe} 
+                       {cnt-b :cnt, qe-b :qe}]
   (or (< cnt-a cnt-b)
-      (and (= cnt-a cnt-b) (< (qe-a) (qe-b)))))
+      (and (= cnt-a cnt-b) (< qe-a qe-b))))
 
 (declare evaluate-branches)
-(declare split-sum-in-two)
+(declare splittable-in-two?)
 
-(defn search-depth [packages current best] 
+(defn search-depth [packages current-solution best] 
   (cond 
-    (= (:pos current) (count packages)) best
-    (not (better-solution? current best)) best
-    (and (= (:spent current) (:goal current)) 
-         (split-sum-in-two packages current)) current
-    :else (evaluate-branches packages current best)))
+    (not (better-solution? current-solution best)) best
+    (and (= (:spent current-solution) (:goal current-solution)) 
+         (splittable-in-two? packages (:els current-solution))) current-solution
+    (= (:pos current-solution) (count packages)) best
+    :else (evaluate-branches packages current-solution best)))
 
-(defn evaluate-branches [packages current best]
-)
+(defn add-next-package [packages 
+                        {:keys [cnt els qe spent pos goal]}]
+  (let [el (packages pos)]
+    {:cnt (inc cnt)
+     :els (conj els el)
+     :qe (* qe el)
+     :spent (+ spent el)
+     :pos (inc pos)
+     :goal goal}))
 
-(defn split-sum-in-two [packages best])
+(defn evaluate-branches [packages {:keys [pos spent goal] :as solution} best]
+  (let [el (packages pos)
+        best (if (> (+ spent el) goal) 
+               best
+               (search-depth packages (add-next-package packages solution) best))]
+    (search-depth packages (update solution :pos inc) best)))   
+
+(defn filter-out-used [res packages elements]
+  (cond
+    (empty? packages) res
+    (empty? elements) (into res packages)
+    :else 
+      (let [[p & ps] packages
+            [e & es] elements]
+        (if (= p e) 
+          (recur res ps es)
+          (recur (conj res p) ps elements))))) 
+
+(declare evaluate-branches-two)
+
+(defn split-sum-in-two [packages 
+                        {:keys [spent pos goal els] :as current-solution}]
+  (cond 
+    (= spent goal) els
+    (= pos (count packages)) false
+    :else (evaluate-branches-two packages current-solution)))
+
+(defn add-next-package-two [packages 
+                            {:keys [els spent pos goal]}]
+  (let [el (packages pos)]
+    {:els (conj els el)
+     :spent (+ spent el)
+     :pos (inc pos)
+     :goal goal}))
+
+(defn evaluate-branches-two [packages {:keys [pos spent goal] :as solution}]
+  (let [el (packages pos)
+        sol (when (<= (+ spent el) goal) 
+               (split-sum-in-two packages (add-next-package-two packages solution)))]
+    (or sol (split-sum-in-two packages (update solution :pos inc)))))   
+
+(defn splittable-in-two? [packages elements]
+  (let [ps (filter-out-used [] packages elements)
+        goal (quot (reduce + ps) 2)
+        one (split-sum-in-two packages {:els [], :goal goal, :pos 0, :spent 0})]
+    (boolean (seq one))))      
 
 (defn get-goal [packages]
   (->> packages 
@@ -38,7 +91,7 @@
        (#(quot % 3))))
 
 (defn day24 
-  ([packages] (search-depth packages {:cnt 0, :els [], :qe 0, :pos 0, :spent 0, :goal (get-goal packages)} {:cnt 10}))
+  ([packages] (search-depth packages {:cnt 0, :els [], :qe 1, :pos 0, :spent 0, :goal (get-goal packages)} {:cnt 1000}))
   ([] (day24 input)))
 
 
